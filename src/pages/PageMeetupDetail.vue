948bff0bbd71a56ffec1df63bcaaff6161187ca4
carolNeux <carolneumeyer@icloud.com>
2021-10-04 16:26:55 -0300

    armamos la navegacion a PageMeetupDetail, trayendo el id con params

--- src/pages/PageMeetupDetail.vue
+++ src/pages/PageMeetupDetail.vue
@@ -3,9 +3,14 @@
 </template>
 <script>
 export default {
- 
+ created(){
+   debugger
+   const meetupId = this.$route.params.id
+ }
 }
 </script>
 <style scoped>
-
+h1 {
+  margin-left: 30px;
+}
 </style>
