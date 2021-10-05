ef8311d053b31d1b889a11aab66c997d42acbc42
carolNeux <carolneumeyer@icloud.com>
2021-10-05 10:37:43 -0300

    hicimos la find page con su ruta

--- src/router/index.js
+++ src/router/index.js
@@ -3,20 +3,29 @@ import Router from 'vue-router'
 
 import PageHome from '@/pages/PageHome'
 import PageMeetupDetail from '@/pages/PageMeetupDetail'
+import PageMeetupFind from '@/pages/PageMeetupFind'
 
 Vue.use(Router)
 
 const router = new Router({
-  routes:[
-    { path: '/',
+  routes: [
+    {
+      path: '/',
       name: 'PageHome',
-      component: PageHome},
-    { path: '/meetups/:id',
+      component: PageHome
+    },
+    {
+      path: '/find',
+      name: 'PageMeetupFind',
+      component: PageMeetupFind
+    },
+    {
+      path: '/meetups/:id',
       name: 'PageMeetupDetail',
       component: PageMeetupDetail
     }
   ],
-  mode:'history'
+  mode: 'history'
 })
 
 export default router
