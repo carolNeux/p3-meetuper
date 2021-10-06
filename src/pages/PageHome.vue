dedc5fe4e51e882bdb88699e9d2892584dca15e0
carolNeux <carolneumeyer@icloud.com>
2021-10-06 16:00:35 -0300

    vuex, modules, lesson132'

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -45,8 +45,8 @@
     },
     computed: {
       ...mapState({ 
-        meetups: state=> state.meetups,
-        categories: state=> state.categories
+        meetups: state=> state.meetups.items,
+        categories: state=> state.categories.items
       })
     },
     created () {
@@ -54,7 +54,8 @@
       this.fetchCategories()
     },
     methods:{
-      ...mapActions (['fetchMeetups', 'fetchCategories'])
+      ...mapActions ('meetups', ['fetchMeetups']),
+      ...mapActions ('categories', ['fetchCategories'])
     }
   }
 </script>
